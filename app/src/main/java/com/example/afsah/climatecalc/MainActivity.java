f3a8f8539c57e20d8ab36105cfc6285e94b3d6de
afsahulsyed <afsahulsyed@gmail.com>
2017-03-02 12:41:36 +0530

    put functions in mainactivity

--- app/src/main/java/com/example/afsah/climatecalc/MainActivity.java
+++ app/src/main/java/com/example/afsah/climatecalc/MainActivity.java
@@ -1,5 +1,16 @@
 package com.example.afsah.climatecalc;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,11 +18,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener  {
 
     Button button1;
+    Button button2;
 
     TextView textView2;
     TextView textView4;
@@ -26,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
 
     EditText editText;
 
+    double lat = 6.9;
+    double lon = 6.9;
+
+    getLocation getLocation_obj;
+    private GoogleApiClient mGoogleApiClient;
+
    // URL url;
 
     String place;
@@ -36,11 +58,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         activateUI();
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
+        getLocation_obj = new getLocation(mGoogleApiClient);
     }
     public void activateUI(){
 
         button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
         textView2 = (TextView)findViewById(R.id.textView2);
         textView4 = (TextView)findViewById(R.id.textView4);
         textView6 = (TextView)findViewById(R.id.textView6);
@@ -93,5 +124,85 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                onButtonClick(view);
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        System.out.println("inside onConnected***************");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("no permission****************");
+
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            lat = mLastLocation.getLatitude();
+            lon = mLastLocation.getLongitude();
+            System.out.println("obj **********************"+ mLastLocation);
+            System.out.println("lat**********************" + mLastLocation.getLatitude());
+            System.out.println("lon***********************" + mLastLocation.getLongitude());
+            //lat.setText(String.valueOf(mLastLocation.getLatitude()));
+            //lon.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    private boolean isGPSEnabled() {
+        LocationManager cm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return cm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    public void onButtonClick(View v){
+        System.out.println("inside button click ***********");
+        if(v.getId() == R.id.button2){
+
+            System.out.println("in gps button ***************");
+            if(!isGPSEnabled()){
+                new AlertDialog.Builder(this)
+                        .setMessage("Please activate your GPS Location!")
+                        .setCancelable(false)
+                        .setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(i);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .show();
+            } else {
+                //textLat.setText(String.valueOf(lat));
+                //textLong.setText(String.valueOf(lon));
+
+                System.out.println("lat**********************" +  lat);
+                System.out.println("lon***********************" + lon);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/afsah/climatecalc/getLocation.java
+++ app/src/main/java/com/example/afsah/climatecalc/getLocation.java
@@ -1,7 +1,5 @@
 package com.example.afsah.climatecalc;
 
-import android.*;
-import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -13,7 +11,9 @@ import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -24,48 +24,36 @@ import com.google.android.gms.location.LocationServices;
  * Created by afsah on 28-Feb-2017.
  */
 
-public class getLocation extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class getLocation extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private GoogleApiClient mGoogleApiClient;
     EditText textLat;
     EditText textLong;
     EditText lat;
     EditText lon;
 
-    getLocation() {
+
+    getLocation(GoogleApiClient mGoogleApiClient) {
         // textLat = (EditText) findViewById(R.id.latitude);
         //textLong = (EditText) findViewById(R.id.longitude);
 
-        if (mGoogleApiClient == null) {
+        /*if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
-        }
+        }*/
+        this.mGoogleApiClient = mGoogleApiClient;
+
+
+
     }
 
-    private boolean isGPSEnabled() {
-        LocationManager cm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return cm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-    }
+
 
     @Override
     public void onConnected(Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            lat.setText(String.valueOf(mLastLocation.getLatitude()));
-            lon.setText(String.valueOf(mLastLocation.getLongitude()));
-        }
+
     }
 
     @Override
@@ -78,30 +66,5 @@ public class getLocation extends ActionBarActivity implements GoogleApiClient.Co
 
     }
 
-    public void onButtonClick(View v){
-        if(v.getId() == R.id.button2){
 
-            System.out.println("in gps button ***************");
-            if(!isGPSEnabled()){
-                new AlertDialog.Builder(this)
-                        .setMessage("Please activate your GPS Location!")
-                        .setCancelable(false)
-                        .setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                                startActivity(i);
-                            }
-                        })
-                        .setNegativeButton("Cancel", null)
-                        .show();
-            } else {
-                textLat.setText(String.valueOf(lat));
-                textLong.setText(String.valueOf(lon));
-
-                System.out.println("lat**********************" + lat);
-                System.out.println("lon***********************" + lon);
-            }
-
-        }
-    }
 }
